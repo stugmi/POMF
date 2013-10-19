d0f9776e736d8bce6bee1b27b1cf4d8272365d5c
wahpomf <biggygibson@gmail.com>
2013-10-19 23:24:38 +0200

    Update pomf.py

--- pomf.py
+++ pomf.py
@@ -23,10 +23,40 @@ except ImportError:
 import os
 import subprocess
 import time
+from sys import argv
+import getopt
 
+### Edit ###
 screenshot_dir = os.getenv("HOME") + os.path.sep + "screenshots" + os.path.sep
 image_directory = "http://a.pomf.se/"
 upload_script = "http://pomf.se/upload.php"
+############
+
+def main():
+	try:
+		if not os.path.exists(screenshot_dir):
+			os.makedirs(screenshot_dir)
+
+	except Exception as e:
+		notify("Error creating directory {0}, {1}".format(screenshot_dir), e)
+		exit()
+
+	try:
+		response = requests.post(
+			url=upload_script,
+			files={"files[]":open(file_upload, "r")}
+		)
+	except Exception as e:
+		notify("Error uploading {0}".format(e))
+		exit()
+
+	response = response.text.split('"')
+	response_text = response[9]
+
+	notify(image_directory + response_text)
+	clipboard(image_directory + response_text)
+	print image_directory + response_text
+
 
 def notify(message):
 	pynotify.init(message)
@@ -41,30 +71,32 @@ def clipboard(message):
 	)
 	p.stdin.write(message)
 
-try:
-	if not os.path.exists(screenshot_dir):
-		os.makedirs(screenshot_dir)
-
-except Exception as e:
-	notify("Error creating directory {0}, {1}".format(screenshot_dir), e)
-	exit()
-
-screenshot_name = "{0}{1}.png".format(screenshot_dir, int(time.time()))
-
-p = subprocess.Popen(["scrot", "--select", screenshot_name])
-p.wait()
+def Usage():
+    print "Usage: pomf [OPTION]... [FILE]\n"
+    print "  -f, --file \tUploads a file to pomf.se"
+    print "  -i, --image \tUploads a image using scrot --select and uploads the image to pomf.se"
+    print "\n Example:\n\t pomf -f pomf.flac"
+    print " \t Uploads a file to pomf.se notifies you once it's done and the link is saved to your clipboard.\n"
+    exit()	
 
 try:
-	response = requests.post(
-		url=upload_script,
-		files={"files[]":open(screenshot_name, "r")}
-	)
-except Exception as e:
-	notify("Error uploading {0}".format(e))
-	exit()
+	opts, args = getopt.getopt(argv[1:], "hif", ["help", "image","file="])
+except getopt.GetoptError, err:
+	print("Error! Argument is invalid\n")
+	Usage()
 
-response = response.text.split('"')
-response_text = response[9]
+for o, a in opts:
+	if o in ("-h","--help"):
+		Usage()
+	elif o in ("-i","--image"):
+		file_upload = "{0}{1}.png".format(screenshot_dir, int(time.time()))
+		p = subprocess.Popen(["scrot", "--select", file_upload])
+		p.wait()
+	elif o in ("-f","--file"):
+		file_upload = argv[2]
+	else:
+		Usage()
 
-notify(image_directory + response_text)
-clipboard(image_directory + response_text)
+
+if __name__ == "__main__":
+   main()
